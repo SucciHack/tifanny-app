262f296c4ff2388ef4ff79f70a57887ff75e2e25
Kaweesi Samuel <158592803+SucciHack@users.noreply.github.com>
2025-01-11 22:00:05 +0300

    removed rule

--- components/Herocard.jsx
+++ components/Herocard.jsx
@@ -8,7 +8,7 @@ export default function Herocard() {
             <div className="flex items-center gap-4">
                 <span className="flex-shrink-0 flex items-center justify-center rounded-full bg-blue-500 p-2 text-white">
                 <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4">
-                    <path clipRule="evenodd" d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z" fill-rule="evenodd"></path>
+                    <path clipRule="evenodd" d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z" fill-Rule="evenodd"></path>
                 </svg>
                 </span>
                 <p className="font-semibold text-white">New message!</p>
